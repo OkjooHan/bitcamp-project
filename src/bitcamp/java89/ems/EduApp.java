3ac8c2f2383477499a3397a0abdbb55079755cf9
OkjooHan <okjoo.han88@gmail.com>
2016-10-28 11:16:01 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -3,106 +3,63 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class EduApp {
-  static Student[] students = new Student[100];
-  static int length = 0;
-  static Scanner keyScan = new Scanner(System.in);
-
   public static void main(String[] args) {
-    System.out.println("비트캠프 관리시스템에 오신걸 환영합니다.");
+    System.out.println("=== 비트캠프 관리시스템에 오신걸 환영합니다 ===");
+    Textbook[] books = new Textbook[5];
+    Scanner keyScan = new Scanner(System.in);
+    int length = 0;
 
     loop:
-    while (true) {
-      System.out.print("명령> ");
-      String command = keyScan.nextLine().toLowerCase();
+    while(true){
+      System.out.printf("## 명령어를 입력해주세요.\n"
+                      + "   (추가: add, 리스트보기: list, 상세정보보기:view, 종료:exit)\n");
+      switch(keyScan.nextLine()) {
+      case "add":
+        if(length >= books.length) {
+          System.out.println("배열이 가득찼습니다.");
+          break;
+        }
+        while(length < books.length) {
+          Textbook book = new Textbook();
+          book.inputBookValue();
+          books[length++] = book;
 
-      switch (command) {
-      case "add": doAdd(); break;
-      case "list": doList(); break;
-      case "view": doView(); break;
-      case "quit":
-        System.out.println("Good bye!");
-        break loop;
+          if(length >= books.length) {
+            System.out.println("배열이 가득찼습니다.");
+            break;
+          } else {
+            System.out.printf("계속 입력하시겠습니까(y/n)? ");
+            if (!keyScan.nextLine().equals("y")) {
+              break;
+            }
+          }
+        }
+        break;
+      case "list":
+        //printBookList(books, length);
+        for (int i = 0; i < length; i++) {
+          books[i].printBook();
+        }
+        break;
+      case "view":
+        boolean nofound = false;
+        System.out.println("## 찾으실 책이름을 입력하세요 : ");
+        String searchTitle = keyScan.nextLine();
+        for (int i = 0; i < length; i++) {
+          if (books[i].title.equals(searchTitle)){
+            books[i].printBook();
+            nofound = true;
+          }
+        }
+        if (!nofound) {System.out.println("결과 값이 없습니다.");}
+        break;
+      case "exit":
+        System.out.print("## 종료하시겠습니까(y/n)? ");
+        if(keyScan.nextLine().equals("y")) {break loop;}
+        else {break;}
       default:
-        System.out.println("지원하지 않는 명령어입니다.");
-      }
-    }
-  }
-
-  static void doList() {
-    for (int i = 0; i < length; i++) {
-      Student student = students[i];
-      System.out.printf("%s,%s,%s,%s,%s,%s,%d,%s\n",
-        student.userId,
-        student.password,
-        student.name,
-        student.tel,
-        student.email,
-        ((student.working)?"yes":"no"),
-        student.birthYear,
-        student.school);
-    }
-  }
-
-  static void doAdd() {
-    // 반복 해서 입력 받는다.
-    while (length < students.length) {
-      Student student = new Student();
-      System.out.print("아이디(:hong)? ");
-      student.userId = keyScan.nextLine();
-
-      System.out.print("암호(예:1111)? ");
-      student.password = keyScan.nextLine();
-
-      System.out.print("이름(예:홍길동)? ");
-      student.name = keyScan.nextLine();
-
-      System.out.print("전화(예:010-1111-2222)? ");
-      student.tel = keyScan.nextLine();
-
-      System.out.print("이메일(예:hong@test.com)? ");
-      student.email = keyScan.nextLine();
-
-      System.out.print("재직중(y/n)? ");
-      student.working = (keyScan.nextLine().equals("y")) ? true : false;
-
-      System.out.print("태어난해(예:1980)? ");
-      student.birthYear = Integer.parseInt(keyScan.nextLine());
-
-      System.out.print("최종학교(예:비트고등학교)? ");
-      student.school = keyScan.nextLine();
-
-      students[length++] = student;
-
-      System.out.print("계속 입력하시겠습니까(y/n)? ");
-      if (!keyScan.nextLine().equals("y"))
-        break;
-    }
-  }
-
-  static void doView() {
-    System.out.print("조회할 학생의 아이디는? ");
-    String userId = keyScan.nextLine().toLowerCase();
-    for (int i = 0; i < length; i++) {
-      if (students[i].userId.toLowerCase().equals(userId)) {
-        System.out.printf("아이디: %s\n", students[i].userId);
-        System.out.printf("암호: (***)\n");
-        System.out.printf("이름: %s\n", students[i].name);
-        System.out.printf("전화: %s\n", students[i].tel);
-        System.out.printf("이메일: %s\n", students[i].email);
-        System.out.printf("재직중: %s\n", (students[i].working) ? "Yes" : "No");
-        System.out.printf("태어난 해: %d\n", students[i].birthYear);
-        System.out.printf("학교: %s\n", students[i].school);
-        break;
+        System.out.println("올바른 명령어가 아닙니다. 다시 입력해주세요.");
       }
     }
   }
 }
-
-/*
-
-
-
-
-
-
-*/
