11252062c01b6525a52693ec32d4525ba86c1c4c
OkjooHan <okjoo.han88@gmail.com>
2016-10-28 11:11:03 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -3,76 +3,106 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class EduApp {
+  static Student[] students = new Student[100];
+  static int length = 0;
+  static Scanner keyScan = new Scanner(System.in);
+
   public static void main(String[] args) {
     System.out.println("비트캠프 관리시스템에 오신걸 환영합니다.");
 
-    //1) 사용자로부터 값을 입력받을 떄
-    Scanner keyScan = new Scanner(System.in);
+    loop:
+    while (true) {
+      System.out.print("명령> ");
+      String command = keyScan.nextLine().toLowerCase();
 
-    Textbook[] books = new Textbook[5];
-    int length = 0;
+      switch (command) {
+      case "add": doAdd(); break;
+      case "list": doList(); break;
+      case "view": doView(); break;
+      case "quit":
+        System.out.println("Good bye!");
+        break loop;
+      default:
+        System.out.println("지원하지 않는 명령어입니다.");
+      }
+    }
+  }
 
-    Textbook book = null; //주소 없음을 의미한다.
-    //while 문 안에서 Textbook book = new Textbook();
-    //해도 동일하게 book 하나만 생성하여 사용하지만, while문 밖에서 선언하면
-    //while문 실행 후에도 계속 사용할 수 있다.
-    //=> while문 내에서만 사용할 거라면 안에서 선언하여 사용하면 된다.
-    //=> best practice에서는 가장 작은 범위에서 선언하여 사용하기를 권장한다.
-    while(length < books.length) {
-      book = new Textbook();
-      //Textbook book = new Textbook(); 동일함.
+  static void doList() {
+    for (int i = 0; i < length; i++) {
+      Student student = students[i];
+      System.out.printf("%s,%s,%s,%s,%s,%s,%d,%s\n",
+        student.userId,
+        student.password,
+        student.name,
+        student.tel,
+        student.email,
+        ((student.working)?"yes":"no"),
+        student.birthYear,
+        student.school);
+    }
+  }
 
-      System.out.print("강좌명(예:자바&DB)? ");
-      book.className = keyScan.nextLine();
+  static void doAdd() {
+    // 반복 해서 입력 받는다.
+    while (length < students.length) {
+      Student student = new Student();
+      System.out.print("아이디(:hong)? ");
+      student.userId = keyScan.nextLine();
 
-      System.out.print("교재명(예:자바프로그래밍)? ");
-      book.title = keyScan.nextLine();
+      System.out.print("암호(예:1111)? ");
+      student.password = keyScan.nextLine();
 
-      System.out.print("저자(예:홍길동)? ");
-      book.author = keyScan.nextLine();
+      System.out.print("이름(예:홍길동)? ");
+      student.name = keyScan.nextLine();
 
-      System.out.print("출판사(예:비트출판사)? ");
-      book.press = keyScan.nextLine();
+      System.out.print("전화(예:010-1111-2222)? ");
+      student.tel = keyScan.nextLine();
 
-      System.out.print("가격(예:30000)? ");
-      book.price = Integer.parseInt(keyScan.nextLine());
+      System.out.print("이메일(예:hong@test.com)? ");
+      student.email = keyScan.nextLine();
 
-      System.out.print("쪽수(예:348)? ");
-      book.pages = Integer.parseInt(keyScan.nextLine());
+      System.out.print("재직중(y/n)? ");
+      student.working = (keyScan.nextLine().equals("y")) ? true : false;
 
-      System.out.print("재고수량(예:32)? ");
-      book.stock = Integer.parseInt(keyScan.nextLine());
+      System.out.print("태어난해(예:1980)? ");
+      student.birthYear = Integer.parseInt(keyScan.nextLine());
 
-      System.out.print("부록(y/n)? ");
-      book.suppl = (keyScan.nextLine().equals("y") ? true : false);
+      System.out.print("최종학교(예:비트고등학교)? ");
+      student.school = keyScan.nextLine();
 
-      System.out.print("배부여부(y/n)? ");
-      book.distr = (keyScan.nextLine().equals("y") ? true : false);
+      students[length++] = student;
 
-      books[length++] = book;
+      System.out.print("계속 입력하시겠습니까(y/n)? ");
+      if (!keyScan.nextLine().equals("y"))
+        break;
+    }
+  }
 
-      System.out.printf("계속 입력하시겠습니까(y/n)? ");
-      if (!keyScan.nextLine().equals("y")) {
+  static void doView() {
+    System.out.print("조회할 학생의 아이디는? ");
+    String userId = keyScan.nextLine().toLowerCase();
+    for (int i = 0; i < length; i++) {
+      if (students[i].userId.toLowerCase().equals(userId)) {
+        System.out.printf("아이디: %s\n", students[i].userId);
+        System.out.printf("암호: (***)\n");
+        System.out.printf("이름: %s\n", students[i].name);
+        System.out.printf("전화: %s\n", students[i].tel);
+        System.out.printf("이메일: %s\n", students[i].email);
+        System.out.printf("재직중: %s\n", (students[i].working) ? "Yes" : "No");
+        System.out.printf("태어난 해: %d\n", students[i].birthYear);
+        System.out.printf("학교: %s\n", students[i].school);
         break;
       }
     }
-    printBookList(books, length);
-  }
-
-  static void printBookList(Textbook[] books, int length) {
-    Textbook book = null;
-    for (int i = 0; i < length; i++) {
-      book = books[i];
-      System.out.printf("%s, %s, %s, %s, %d원, %d쪽, %d남음, %s, %s\n",
-        book.className,
-        book.title,
-        book.author,
-        book.press,
-        book.price,
-        book.pages,
-        book.stock,
-        (book.suppl ? "부록있음" : "부록없음"),
-        (book.distr ? "배부함" : "배부안함"));
-    }
   }
 }
+
+/*
+
+
+
+
+
+
+*/
